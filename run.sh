9819e6c53a35af98ce4d7f1ee4dd231c0a425609
WilliamMN <w1lliam.m4rques@gmail.com>
2022-05-02 11:33:38 -0300

    shell script para rodar a aplicação

--- run.sh
+++ run.sh
@@ -1,1 +1,14 @@
-npm start
+if [ -d "./node_modules" ]; then
+  echo "removendo node modules"
+  rm -v -r node_modules
+fi
+
+echo "importando as dependencias"
+npm i
+
+read -p "Iniciar a aplicação? " input
+
+if [ "${input}" = "sim" ]; then
+  clear
+  npm start
+fi
